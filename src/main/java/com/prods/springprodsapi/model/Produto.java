6b00391047fb94f04c9294a4cef364b0dfb63246
Victor Goulart <victorfgbr@gmail.com>
2020-09-12 02:33:26 -0300

    Bandeira + Tipo unique on Produto

--- src/main/java/com/prods/springprodsapi/model/Produto.java
+++ src/main/java/com/prods/springprodsapi/model/Produto.java
@@ -8,25 +8,26 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name="produto")
+@Table(name = "produto", uniqueConstraints = @UniqueConstraint(columnNames = { "id_bandeira", "id_tipo" }))
 public class Produto {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE)
-	@Column(name="id_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name = "id_produto")
 	public Long id;
 
-	@ManyToOne ()
-	@JoinColumn(name="id_bandeira")
+	@ManyToOne()
+	@JoinColumn(name = "id_bandeira")
 	public Bandeira bandeira;
-	
-	@ManyToOne ()
-	@JoinColumn(name="id_tipo")
+
+	@ManyToOne()
+	@JoinColumn(name = "id_tipo")
 	public Tipo tipo;
 
 	public String nome;
